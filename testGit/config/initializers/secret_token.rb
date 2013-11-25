f5312552e9e618a5501380fd6b4630503057ea30
syf <shenyunfeng@126.com>
2013-11-25 16:48:12 +0800

    haha

---
+++ testGit/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestGit::Application.config.secret_token = '22310d44e1625a10b4649089f6d5ef29e0d85fa2f7bedfc94fc4b6948ddfa3b16713fc6c18210357b4bdb57d390e95bb55d015b36b65769ecc7122d4d6402d37'
